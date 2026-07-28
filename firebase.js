6bb2b014612f18dc174019d82efd27ec2957ce10
gabrieladolar7-stack <gabrieladolar7@gmail.com>
2026-07-28 16:41:46 -0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,9 +1,7 @@
 // ===============================
 // FIREBASE
 // ===============================
-
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-
 import {
     getDatabase,
     ref,
@@ -11,42 +9,24 @@ import {
     set,
     onValue
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
-
 const firebaseConfig = {
-
-    apiKey: "AIzaSyDqdpAchewxlYmGrhFb5WyJKqbL5UP2XsA",
-
-    authDomain: "tikfinity-overlay.firebaseapp.com",
-
-    databaseURL: "https://tikfinity-overlay-default-rtdb.firebaseio.com",
-
-    projectId: "tikfinity-overlay",
-
-    storageBucket: "tikfinity-overlay.firebasestorage.app",
-
-    messagingSenderId: "943639302610",
-
-    appId: "1:943639302610:web:5e747094f2f6151b70380b"
-
+    apiKey: "AIzaSyDcqposPDuzcIdyx9qwxsmir-1j8Yv4sxA",
+    authDomain: "tikfinity-overlay-e879d.firebaseapp.com",
+    databaseURL: "https://tikfinity-overlay-e879d-default-rtdb.firebaseio.com",
+    projectId: "tikfinity-overlay-e879d",
+    storageBucket: "tikfinity-overlay-e879d.firebasestorage.app",
+    messagingSenderId: "143473145353",
+    appId: "1:143473145353:web:b1849585a981e84a59aad8"
 };
-
 // Inicializar Firebase
 const app = initializeApp(firebaseConfig);
-
 // Base de datos
 const db = getDatabase(app);
-
 // Exportar funciones
 export {
-
     db,
-
     ref,
-
     get,
-
     set,
-
     onValue
-
 };
